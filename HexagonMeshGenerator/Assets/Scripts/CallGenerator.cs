0fc2c5e87d3c3205ded213036aa02e3c3451ecb1
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2019-09-18 19:05:59 +0200

    button

---
+++ HexagonMeshGenerator/Assets/Scripts/CallGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CallGenerator : MonoBehaviour
+{
+    public Button startGeneration;
+    public GridGenerator other;
+
+    void Start()
+    {
+        Button btn = startGeneration.GetComponent<Button>();
+        btn.onClick.AddListener(startFunctions);
+    }
+
+    public void startFunctions()
+    {
+        other.AddGap();
+        other.CalcStartPos();
+        other.CreateGrid();
+    }
+
+}
---
+++ HexagonMeshGenerator/Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GridGenerator : MonoBehaviour
+{
+    public Transform hexPrefab;
+
+    public int gridWidth = 11;
+    public int gridHeight = 11;
+
+    float hexWidth = 1.732f;
+    float hexHeight = 2.0f;
+    public float gap = 0.0f;
+
+    Vector3 startPos;
+
+    public void AddGap()
+    {
+        hexWidth += hexWidth * gap;
+        hexHeight += hexHeight * gap;
+    }
+
+    public void CalcStartPos()
+    {
+        float offset = 0;
+        if (gridHeight / 2 % 2 != 0)
+            offset = hexWidth / 2;
+
+        float x = -hexWidth * (gridWidth / 2) - offset;
+        float z = hexHeight * 0.75f * (gridHeight / 2);
+
+        startPos = new Vector3(x, 0, z);
+    }
+
+    Vector3 CalcWorldPos(Vector2 gridPos)
+    {
+        float offset = 0;
+        if (gridPos.y % 2 != 0)
+            offset = hexWidth / 2;
+
+        float x = startPos.x + gridPos.x * hexWidth + offset;
+        float z = startPos.z - gridPos.y * hexHeight * 0.75f;
+
+        return new Vector3(x, 0, z);
+    }
+
+    public void CreateGrid()
+    {
+        for (int y = 0; y < gridHeight; y++)
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                Transform hex = Instantiate(hexPrefab) as Transform;
+                Vector2 gridPos = new Vector2(x, y);
+                hex.position = CalcWorldPos(gridPos);
+                hex.parent = this.transform;
+                hex.name = "Hexagon" + x + "|" + y;
+            }
+        }
+    }
+}
